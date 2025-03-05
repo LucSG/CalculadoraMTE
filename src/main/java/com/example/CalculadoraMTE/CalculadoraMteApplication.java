15b5c845a98d66635e2c32ac74e99dbd4bb7288d
Lucas Santos Guimarães <lucasguima@yahoo.com.br>
2025-03-04 21:42:19 -0500

    First Commit

---
+++ src/main/java/com/example/CalculadoraMTE/CalculadoraMteApplication.java
@@ -0,0 +1,13 @@
+package com.example.CalculadoraMTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraMteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CalculadoraMTE/CalculadoraMteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CalculadoraMTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraMteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
